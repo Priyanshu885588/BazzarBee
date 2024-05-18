2d2f84a45796455cc8436dcf605319cfaa05ccb6
karthik092003 <karthik.s092003@gmail.com>
2024-05-18 22:19:11 +0530

    added orders schema

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -5,6 +5,7 @@ const {
   removeCart,
   getAllCartProducts,
   clearCart,
+  storeUserAddress,
 } = require("../controller/ordersController");
 const auth = require("../middleware/auth");
 
@@ -17,5 +18,7 @@ router.route("/clearCart").delete(auth.authenticationMiddleware, clearCart);
 router
   .route("/getcartproducts")
   .get(auth.authenticationMiddleware, getAllCartProducts);
+
+router.route("/address").post(auth.authenticationMiddleware,storeUserAddress);
 
 module.exports = router;
